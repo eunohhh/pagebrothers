db90018d0a830f20d89105862a7e722ed22571e0
Eun Oh <bdohhhhh@gmail.com>
2024-09-06 15:14:06 +0900

    [chore]: inivite > invitation 으로 폴더명 변경

--- apps/invitation/src/app/layout.tsx
+++ apps/invitation/src/app/layout.tsx
@@ -6,8 +6,8 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-    title: 'Invite',
-    description: 'Invite',
+    title: 'Invitation',
+    description: 'Invitation',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }): JSX.Element {
--- apps/invitation/src/app/page.tsx
+++ apps/invitation/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page(): JSX.Element {
     return (
         <main className="flex flex-col items-center justify-between min-h-screen p-24">
-            <h1>Invite</h1>
+            <h1>Invitation</h1>
         </main>
     );
 }
